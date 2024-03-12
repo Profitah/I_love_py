46d0d2dbc52a896563d56629ba01cb7e5068e159
I, yoonah <qwer120542@gmail.com>
2024-03-12 22:36:01 +0900

    대소문자 변경

--- 2024/3월/240312/10808.py
+++ 2024/3월/240312/10808.py
@@ -1,6 +1,6 @@
-s = list(str(input()))  
+S = list(str(input()))  
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 for i in range(len(alphabet)):
-    x = s.count(alphabet[i])  
+    x = S.count(alphabet[i])  
     print(x, end=' ')
